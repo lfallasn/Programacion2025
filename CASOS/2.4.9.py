142113bc6a74d20a23ec21c63f65da8856cd1fd3
lfallasn <lfallasn@ctpplatanares.ed.cr>
2025-05-30 16:17:39 -0600

    semana 2 de python

---
+++ CASOS/2.4.9.py
@@ -0,0 +1,10 @@
+kilometers = 12.25
+miles = 7.38
+
+s = miles * 1.61
+s = kilometers / 1.61
+
+
+print(miles, "millas son", round(miles_to_kilometers, 2), "kilómetros")
+print(kilometers, "kilómetros son", round(kilometers_to_miles, 2), "millas")
+ 
